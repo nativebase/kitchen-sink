865c903abcc4fa80bae9d9655aa93671717f0013
Amar Somani <amars@geekyants.com>
2021-05-24 18:34:57 +0530

    basic layout with some components

--- src/config/map.tsx
+++ src/config/map.tsx
@@ -83,4 +83,372 @@ export const mapping = {
 			},
 		],
 	},
+	CheckBox: {
+		title: "CheckBox",
+		description: "Primitive Element",
+		components: [
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Checkbox/basic"),
+				title: "Basic",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Checkbox/checkboxGroup"),
+				title: "CheckBox",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Checkbox/controlledCheckbox"),
+				title: "CheckBox",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Checkbox/customColor"),
+				title: "CheckBox",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Checkbox/customIcon"),
+				title: "CheckBox",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Checkbox/disabled"),
+				title: "CheckBox",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Checkbox/FormControlled"),
+				title: "CheckBox",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Checkbox/invalid"),
+				title: "CheckBox",
+				description: "",
+			},
+			// {
+			// 	component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Checkbox/playground"),
+			// 	title: "CheckBox",
+			// 	description: "",
+			// },
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Checkbox/size"),
+				title: "CheckBox",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Checkbox/uncontrolledCheckbox"),
+				title: "CheckBox",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Checkbox/withRef"),
+				title: "CheckBox",
+				description: "",
+			},
+		],
+	},
+
+	Column: {
+		title: "Column",
+		description: "Renders your data vertically",
+		components: [
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Column/Basic"),
+				title: "Basic",
+				description: "",
+			},
+		],
+	},
+	Flex: {
+		title: "Flex",
+		description: "Box with display='Flex'",
+		components: [
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Flex/basic"),
+				title: "Basic",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Flex/spacer"),
+				title: "Basic",
+				description: "",
+			},
+		],
+	},
+	Heading: {
+		title: "Heading",
+		description: "It shows the Title",
+		components: [
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Heading/Basic"),
+				title: "Basic",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Heading/Composition"),
+				title: "Composition",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Heading/OverridenStyle"),
+				title: "OverridenStyle",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Heading/Sizes"),
+				title: "Sizes",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Heading/Truncate"),
+				title: "Truncate",
+				description: "",
+			},
+		],
+	},
+	HStack: {
+		title: "HStack",
+		description: "Rows structure",
+		components: [
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/HStack/basic"),
+				title: "Basic",
+				description: "",
+			},
+		],
+	},
+	Icon: {
+		title: "Icon",
+		description: "Show Icon in NativeBase",
+		components: [
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Icon/AllIcons"),
+				title: "Basic",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Icon/Basic"),
+				title: "Basic",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Icon/CreateIcon"),
+				title: "Basic",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Icon/CustomIcon"),
+				title: "Basic",
+				description: "",
+			},
+		],
+	},
+	Image: {
+		title: "Image",
+		description: "Show image in NativeBase",
+		components: [
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Image/Basic"),
+				title: "Basic",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Image/BorderRadius"),
+				title: "BorderRadius",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Image/FallbackSupport"),
+				title: "FallbackSupport",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Image/Sizes"),
+				title: "Sizes",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Image/WithRef"),
+				title: "WithRef",
+				description: "",
+			},
+		],
+	},
+	Input: {
+		title: "Input",
+
+		description: "",
+		components: [
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Input/Basic"),
+				title: "Basic Input",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Input/Controlled"),
+				title: "Controlled Input",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Input/Elements"),
+				title: "Elements",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Input/FormControlled"),
+				title: "FormControlled",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Input/Masked"),
+				title: "Masked",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Input/Size"),
+				title: "Size",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Input/Variant"),
+				title: "Variant",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Input/Addons"),
+				title: "Addons",
+				description: "",
+			},
+		],
+	},
+	Link: {
+		title: "Link",
+		description: "Used to navigate",
+		components: [
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Link/Basic"),
+				title: "Basic",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Link/CompositeLink"),
+				title: "Composite Link",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Link/CustomOnPress"),
+				title: "Custom Pressable link",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Link/ExternalLink"),
+				title: "External link",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Link/UnderlineLink"),
+				title: "Underline Link",
+				description: "",
+			},
+		],
+	},
+	List: {
+		title: "List",
+		description: "",
+		components: [
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/List/Basic"),
+				title: "Basic",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/List/OrderedList"),
+				title: "Ordered list",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/List/UnorderedList"),
+				title: "Unordered list",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/List/ListWithIcon"),
+				title: "ListWithIcon",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/List/PressableList"),
+				title: "Pressable list",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/List/StylingList"),
+				title: "Styling list",
+				description: "",
+			},
+			{
+				component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/List/VirtualizedList"),
+				title: "Virtualized list",
+				description: "",
+			},
+		],
+		Radio: {
+			title: "Radio",
+			description: "",
+			components: [
+				{
+					component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Radio/"),
+					title: "Virtualized list",
+					description: "",
+				},
+				{
+					component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Radio/"),
+					title: "Virtualized list",
+					description: "",
+				},
+				{
+					component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Radio/"),
+					title: "Virtualized list",
+					description: "",
+				},
+				{
+					component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Radio/"),
+					title: "Virtualized list",
+					description: "",
+				},
+				{
+					component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Radio/"),
+					title: "Virtualized list",
+					description: "",
+				},
+				{
+					component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Radio/"),
+					title: "Virtualized list",
+					description: "",
+				},
+				{
+					component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Radio/"),
+					title: "Virtualized list",
+					description: "",
+				},
+				{
+					component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Radio/"),
+					title: "Virtualized list",
+					description: "",
+				},
+				{
+					component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Radio/"),
+					title: "Virtualized list",
+					description: "",
+				},
+				{
+					component: require("../../nb/NativeBase/example/storybook/stories/components/primitives/Radio/"),
+					title: "Virtualized list",
+					description: "",
+				},
+			],
+		},
+	},
 };
--- src/screens/ComponentScreen/index.tsx
+++ src/screens/ComponentScreen/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { ScrollView } from "react-native";
 import { Header } from "../../components/Header";
-import { Heading, Text, VStack, Box } from "../../../nb/NativeBase/src";
+import {
+	Heading,
+	Text,
+	VStack,
+	Box,
+	useColorMode,
+} from "../../../nb/NativeBase/src";
 import { mapping } from "../../config/map";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { Divider } from "native-base";
 
 export function Examples({
 	route,
@@ -17,17 +22,21 @@ export function Examples({
 
 	//@ts-ignore
 	const component = mapping[name];
-
+	console.log(component);
+	const { colorMode, toggleColorMode } = useColorMode();
 	return (
 		<Box bg="white">
 			<Header title={component.title} navigation={navigation} />
-			<Text my={1}>{component.description}</Text>
+
 			<ScrollView>
-				<VStack space={6} p={2}>
+				<VStack space={2} p={2} mb={24}>
+					<Text my={10} fontSize="xl">
+						{component.description}
+					</Text>
 					{component.components.map((element: any, index: number) => (
 						<VStack space={2} key={index}>
 							<Heading>{element.title}</Heading>
-							<Divider />
+							{/* <Divider /> */}
 							<element.component.Example />
 							<Text>{element.description}</Text>
 						</VStack>
